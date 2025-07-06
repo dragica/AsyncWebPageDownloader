0ff1f54c205f650ec8beb2f57104e85418c26297
Dragica Nikolova <dragican@live.com>
2025-07-06 19:07:19 +0200

    Update file format.

--- AsyncWebPageDownloader/DownloadConfiguration.cs
+++ AsyncWebPageDownloader/DownloadConfiguration.cs
@@ -5,7 +5,7 @@ namespace AsyncWebPageDownloader
     public class DownloadConfiguration : IDownloadConfiguration
     {
         private const string DefaultInputFolderName = "Content";
-        private const string DefaultInputFileName = "urls.csv";
+        private const string DefaultInputFileName = "urls.txt";
         private const string DefaultOutputFolderName = "Downloads";
         public string UrlsFilePath => Path.Combine(AppContext.BaseDirectory, DefaultInputFolderName, DefaultInputFileName);
         public string OutputFolderPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), DefaultOutputFolderName);
